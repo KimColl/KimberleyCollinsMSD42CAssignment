b260057c553f04e2476c88aab5ef1881bb66d464
KimColl <73173379+KimColl@users.noreply.github.com>
2020-12-23 20:33:06 +0100

    testing

---
+++ Home/2DCar/Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //variables that can be edited from Unity
+    [SerializeField] float moveSpeed = 0.02f;
+
+    [SerializeField] float padding = 0.7f;
+
+    float xMin, xMax, yMin, yMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpMoveBoundaries();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    //to setup the boundaries according to the camera
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+
+        //These are the minimum and maximum position of the camera
+        //xMin = 0, xMax = 1, yMin = 0, yMax = 1;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+
+    //to be able to move the Car player
+    private void Move()
+    {
+        // deltaX will contains the difference in which the Player moves
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+
+        var newXPos = transform.position.x + deltaX;
+
+        //clamp this position newXPos between 0 and 1 on the camera view and not below or more than 0 or 1
+        //clamp the position between 0 and 1 and update the new position
+        //clamps the newXPos between xMin and xMax
+        newXPos = Mathf.Clamp(newXPos, xMin, xMax);
+
+        this.transform.position = new Vector2(newXPos, transform.position.y);
+    }
+}
---
+++ Home/2DCar/Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour
+{
+    [SerializeField] float backgroundScrollSpeed = 0.02f;
+
+    //the Material from the texture background
+    Material myMaterial;
+
+    //movement offSet x = 0; y = 0
+    Vector2 offSet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get the Material of the background from Renderer component
+        myMaterial = GetComponent<Renderer>().material;
+
+        //move in the y-axis at the given speed 
+        offSet = new Vector2(0f, backgroundScrollSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //to update at that offset
+        //move the texture of the material by offset every frame
+        myMaterial.mainTextureOffset += offSet * Time.deltaTime;
+    }
+}
